9aa4d303f4d5cb9d954427da6271958151af1c62
Krzysztof Jusiak <krzysztof@jusiak.net>
2015-05-13 16:25:58 +0100

    dagger2 example

--- tests/complex/dagger2.java
+++ tests/complex/dagger2.java
@@ -1,6 +1,37 @@
 import javax.inject.Singleton;
 import dagger.*;
 
+//@Component(modules = {ApplicationModule.class})
+//@Singleton
+//interface ApplicationComponent {
+	//Tester getTester();
+//}
+//
+//
+//
+
+interface Tester {
+	void gogo();
+}
+
+class TesterImpl implements Tester {
+
+	private int i;
+	@Override
+	public void gogo() {
+		i = 1;
+	}
+}
+
+@Module
+class ApplicationModule {
+	@Provides
+	@Singleton
+	Tester provideTester() {
+		return new TesterImpl();
+	}
+}
+
 @Component(modules = {ApplicationModule.class})
 @Singleton
 interface ApplicationComponent {
